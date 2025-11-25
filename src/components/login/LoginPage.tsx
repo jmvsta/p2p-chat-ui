22570c2111c68c73ffaf0a222eeeb046b2d95641
dmvstv_v <dmvstv_v@gmail.com>
2025-11-25 21:02:13 +0300

    tmp

--- src/components/login/LoginPage.tsx
+++ src/components/login/LoginPage.tsx
@@ -39,7 +39,6 @@ const LoginPage: React.FC<Props> = (props) => {
             settingsService.updateCurrent(login, photo)
         ])
             .then(() => {
-                setApiInited(true);
                 navigate('/servers');
             })
             .catch((error: Error) => {
@@ -64,7 +63,7 @@ const LoginPage: React.FC<Props> = (props) => {
     }
 
     return (
-        <div style={{display: 'flex', flexDirection: 'row', height: '100%', width: '100%'}}>
+        <div style={{display: 'flex', flexDirection: 'row'}}>
             <div style={{width: '50%'}}>
                 <img style={{height: '50%', margin: '0 auto'}} src='/logo.jpg' alt='logo image'/>
                 <Typography sx={{alignSelf: 'center', width: '80%'}} variant='h6' gutterBottom>
--- src/components/video-window/VideoWindow.tsx
+++ src/components/video-window/VideoWindow.tsx
@@ -235,12 +235,15 @@ export default function VideoWindow() {
     // }
 
     return (
-        <div style={{
-            position: 'relative',
-            width: '100%',
-            maxWidth: '100%',
-            height: '100%',
-        }}>
+        <div
+            style={{
+                position: 'relative',
+                width: '100vw',
+                height: '100vh',
+                overflow: 'hidden',
+                background: '#000',
+            }}
+        >
             <video
                 ref={remoteVideo}
                 autoPlay
@@ -248,7 +251,8 @@ export default function VideoWindow() {
                 style={{
                     width: '100%',
                     height: '100%',
-                    objectFit: 'cover',
+                    objectFit: 'contain',
+                    display: 'block',
                     background: '#000',
                 }}
             />
